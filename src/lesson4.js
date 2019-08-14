61f6473fbfd74b7b4b05671599d369852c28ddf6
p-miszczuk <piotr.miszczuk@interia.pl>
2019-08-14 10:08:39 +0200

    lesson5, state

--- src/lesson4.js
+++ src/lesson4.js
@@ -1,24 +1,30 @@
 import React from "react"
 
 class App extends React.Component {
+    
+    constructor() {
+        super()
+        this.state = {
+            txt: "this is the state txt",
+            cat: 0
+        }
+    }
+
+    update (e) {
+        this.setState({txt: e.target.value})
+    }   
+
     render() {
-        let txt = this.props.txt
         return (
             <div>
-                <h1>{txt}</h1>
+                <input type="text" 
+                     onChange={this.update.bind(this)}
+                />
+                <h1>{this.state.txt} - {this.state.cat}</h1>
             </div>
         )
     }
 }
-
-App.propTypes = {
-    txt: React.PropTypes.string,
-    cat: React.PropTypes.number.isRequired
-  }
-  
-  App.defaultProps = {
-    txt: "this is the default text"
-  }
-  
+ 
 
 export default App;
