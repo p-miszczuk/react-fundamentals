29791974c17f48b0ae14be2fc8104516f1a627db
p-miszczuk <piotr.miszczuk@interia.pl>
2019-08-14 10:45:43 +0200

    lesson11, lifecycle methods

--- src/lesson4.js
+++ src/lesson4.js
@@ -1,38 +1,44 @@
-import React from "react"
+import React from 'react';
+import ReactDOM from 'react-dom';
 
 class App extends React.Component {
-    constructor(){
-      super();
-      this.state = {a: ''}
-    }
-    update(){
-      this.setState({
-        a: this.a.refs.input.value,
-        b: this.refs.b.value
-      })
-    }
-    render(){
-      return (
-        <div>
-          <Input
-            ref={ component => this.a = component}
-            update={this.update.bind(this)}
-          /> {this.state.a}
-          <hr />
-          <input
-            ref="b"
-            type="text"
-            onChange={this.update.bind(this)}
-          /> {this.state.b}
-        </div>
-      )
-    }
+  constructor(){
+    super();
+    this.state = {val: 0}
+    this.update = this.update.bind(this)
   }
-  
-  class Input extends React.Component {
-    render(){
-      return <div><input ref="input" type="text" onChange={this.props.update}/></div>
-    }
+  update(){
+    this.setState({val: this.state.val + 1})
   }
+  render(){
+    console.log('render');
+    return <button onClick={this.update}>{this.state.val}</button>
+  }
+  componentDidMount(){
+    console.log('componentDidMount')
+  }
+  componentWillUnmount(){
+    console.log('componentWillUnmount')
+  }
+}
 
-export default App;
+class Wrapper extends React.Component {
+  mount(){
+    ReactDOM.render(<App />, document.getElementById('a'))
+  }
+  unmount(){
+    ReactDOM.unmountComponentAtNode(document.getElementById('a'))
+  }
+  render(){
+    return (
+      <div>
+        <button onClick={this.mount.bind(this)}>Mount</button>
+        <button onClick={this.unmount.bind(this)}>UnMount</button>
+        <div id="a"></div>
+      </div>
+    )
+  }
+}
+
+
+export default Wrapper
