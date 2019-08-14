8d5a48dcb0dec9b57a07a8bfa83cdbc22375f112
p-miszczuk <piotr.miszczuk@interia.pl>
2019-08-14 10:22:58 +0200

    lesson8, propType validation

--- src/lesson4.js
+++ src/lesson4.js
@@ -1,33 +1,23 @@
 import React from "react"
 
 class App extends React.Component {
-    
-    constructor() {
-        super()
-        this.state = {
-            txt: "this is the state txt"
-        }
-    }
 
-    update (e) {
-        this.setState({txt: e.target.value})
-    }   
-
-    render() {
-        return (
-            <div>
-                <Button>I <Heart /> React</Button>
-            </div>
-        )
+    render(){
+        return <Title text="12345678" />
     }
 }
+      
+const Title = (props) => <h1>Title: {props.text}</h1>
 
-const Button = props => <button>{props.children}</button>
-
-class Heart extends React.Component {
-    render(){
-      return <span>&hearts;</span>
+Title.propTypes = {
+text(props, propName, component){
+    if(!(propName in props)){
+    return new Error(`missing ${propName}`)
     }
-  }
+    if(props[propName].length < 6){
+    return new Error(`${propName} was too short`)
+    }
+}
+}
  
 export default App;
